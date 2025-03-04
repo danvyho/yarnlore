51223fc35e544f3dc84baa8bcbc8f146dc09d099
Abe G <abenbg12@gmail.com>
2025-03-04 11:15:46 -0500

    create new post, only missing image.. for later

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
 class PostsController < ApplicationController
+  def index
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to restaurants_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+
+def post_params
+  params.require(:post).permit(:title, :content)
 end
