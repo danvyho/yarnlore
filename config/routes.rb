eab541154420aa8a97bb788ab95174cdbdf76b57
Dan Vy Ho <ho.danvy@gmail.com>
2025-03-04 14:19:25 -0500

    resources issues fixed

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,7 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :update, :destroy]
   end
 
-  get "/posts/:id", to: "posts#show", as: "post"
-  get '/posts/new', to: 'posts#new'
-  post "posts", to: "posts#create"
-  get '/posts/:id/edit', to: 'posts#edit'
   get "/users/:id", to: "users#show", as: "user"
   get "/my_profile", to: "users#my_profile", as: "my_profile"
-  
+
 end
