0bfec1b547a89078c94d64dd0f769311eb52a451
Abe G <abenbg12@gmail.com>
2025-03-04 16:35:09 -0500

    saving create post changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show]
-  
+
   def index
     @posts = Post.all
   end
@@ -48,10 +48,9 @@ end
   private
 
   def post_params
-    params.require(:post).permit(:title, :content)
+    params.require(:post).permit(:title, :content, :photo)
   end
 
  def set_post
    @post = Post.find(params[:id])
  end
-
