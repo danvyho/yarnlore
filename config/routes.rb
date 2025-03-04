1165ad13de62e32190e676157c69015d61bd6450
Jasper Ashgrove <jasperashgrove@gmail.com>
2025-03-04 16:01:53 -0500

    Can now view and post comments. Created several partials, edited routes, and many more.

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,10 @@ Rails.application.routes.draw do
   root "posts#index"
 
   resources :posts do
-    resources :comments, only: [:create, :update, :destroy]
+    resources :comments, only: %I[create update destroy]
   end
 
-  get "/posts/:id", to: "posts#show", as: "post"
-  get '/posts/new', to: 'posts#new'
-  post "posts", to: "posts#create"
-  get '/posts/:id/edit', to: 'posts#edit'
   get "/users/:id", to: "users#show", as: "user"
   get "/my_profile", to: "users#my_profile", as: "my_profile"
-  
+
 end
