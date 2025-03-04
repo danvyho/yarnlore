1e7dcf3c98dbda0cde9971a4fb603ec3fea34e33
Jasper Ashgrove <jasperashgrove@gmail.com>
2025-03-04 11:48:36 -0500

    didn't save, no it is good

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,20 +3,15 @@ class PostsController < ApplicationController
 
   def show
   end
-  
+
   def index
     @posts = Post.all
   end
 
-<<<<<<< HEAD
-  private
-
-  def set_post
-    @post = Post.find(params[:id])
-=======
   def new
     @post = Post.new
   end
+
   def create
     @post = Post.new(post_params)
     if @post.save
@@ -24,15 +19,15 @@ class PostsController < ApplicationController
     else
       render :new, status: :unprocessable_entity
     end
->>>>>>> edb55cf9f7cf59232c7af6688b415c70f1e37eed
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
-
-private
-
-def post_params
-  params.require(:post).permit(:title, :content)
-end
- def set_post
-   @set_post = Post.find(params[:id])
- end
