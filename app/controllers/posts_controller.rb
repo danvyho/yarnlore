64aef26258bca7660d7113df042af03df743262e
Jasper Ashgrove <jasperashgrove@gmail.com>
2025-03-04 11:09:37 -0500

    created basic show page for posts, needs comment controller completed for more work

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @set_post = Post.find(params[:id])
+  end
 end
