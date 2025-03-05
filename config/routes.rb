e299a2f7eba8aa2213ffe7d438323e877ea2fa2d
Lorena Davila <lorena.davila.ermus@gmail.com>
2025-03-05 15:28:08 -0500

    added the create message functionality

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get "/users/:id/chats", to: "chats#index", as: "chats"
     get "/users/:id/chats/:id", to: "chats#show", as: "chat"
-    post "/users/:id/chats/:id", to: "chats#create_message", as: "chat_messages"
+    post "/chats/:id", to: "messages#create", as: "chat_messages"
   end
 
 
