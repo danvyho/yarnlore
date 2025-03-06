c13d8a8c32380bbc2b3b7280029ac67649cc1f6d
Abe G <abenbg12@gmail.com>
2025-03-06 14:26:57 -0500

    Profile account has bio, your posts... only need to style it according to figma

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_05_194358) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_06_184406) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -161,6 +161,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_05_194358) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.text "bio"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
