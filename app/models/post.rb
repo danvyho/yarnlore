82248ce39fdc75c761758b99458bcdc729938f28
Jasper Ashgrove <jasperashgrove@gmail.com>
2025-03-06 12:33:38 -0500

    Created card for show page. Also fixed issue with seeds.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_many :notifications
   has_many :favorites
-  has_many :post_likes
+  has_many :post_likes, dependent: :destroy
   has_one_attached :image
   include PgSearch::Model
   multisearchable against: %i[title content]
