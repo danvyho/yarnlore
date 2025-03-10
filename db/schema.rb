c0a6e22813605f0c8b97127b93921d9eaf8ea45c
Dan Vy Ho <ho.danvy@gmail.com>
2025-03-10 17:03:01 -0400

    adding the count to the followers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_10_183422) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_10_195630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -121,8 +121,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_10_183422) do
     t.boolean "read", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "content"
     t.integer "notification_type"
+    t.text "content"
     t.index ["post_id"], name: "index_notifications_on_post_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
@@ -158,6 +158,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_10_183422) do
     t.string "craft"
     t.string "category"
     t.string "type"
+    t.boolean "pattern"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
