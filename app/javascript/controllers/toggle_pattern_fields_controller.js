c1b3ec3957dc5ac5e9f05a2fc08b22368e5ba462
Lorena Davila <lorena.davila.ermus@gmail.com>
2025-03-11 12:40:47 -0400

    make the accordion open on load if it is a pattern

--- app/javascript/controllers/toggle_pattern_fields_controller.js
+++ app/javascript/controllers/toggle_pattern_fields_controller.js
@@ -2,10 +2,16 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle-pattern-fields"
 export default class extends Controller {
-  static targets = [ "patternFields" ]
+  static targets = [ "radio", "patternFields" ]
 
   connect() {
-
+    this.radioTargets.forEach(radio => {
+      if (radio.value == "pattern" && radio.checked) {
+        this.patternFieldsTarget.classList.remove('visually-hidden');
+        this.patternFieldsTarget.querySelector('button').click()
+      }
+    }
+    );
   }
 
   toggle(event) {
