4d101dd9019816a01d4936a81f77ded288c4c09c
Jasper Ashgrove <jasperashgrove@gmail.com>
2025-03-14 13:05:27 -0400

    descending order notifications now

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @notifications = current_user.notifications
+    @notifications = current_user.notifications.sort_by(&:created_at).reverse
   end
 
 
